442c7706937f4d431c66d856fbf29d210f48888b
Philip Solobay <maximus242@gmail.com>
2021-08-21 22:41:22 -0600

    add the array database migration to persist the 2D array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_144245) do
+ActiveRecord::Schema.define(version: 2021_08_21_035308) do
 
   create_table "boards", force: :cascade do |t|
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_20_144245) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "array"
   end
 
 end
