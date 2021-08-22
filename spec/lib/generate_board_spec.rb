f42a2a50dab8200642c4759a976c8bcd20bf41c6
Philip Solobay <maximus242@gmail.com>
2021-08-21 22:41:18 -0600

    test that the board generates the right height

--- spec/lib/generate_board_spec.rb
+++ spec/lib/generate_board_spec.rb
@@ -22,5 +22,24 @@ RSpec.describe GenerateBoard do
 
       expect(sub_arrays_are_same_width).to be true
     end
+
+    it "generates the board with the correct height for the first sub-array" do
+      board_height = rand(4..100) 
+      generator = described_class.new(width: rand(1..100), height: board_height, number_of_mines: rand(1..4))
+
+      height = generator.board.length 
+
+      expect(height).to eq board_height 
+    end
+
+    it "has all the sub-arrays of the same height" do
+      # Make the width and height at least 25x25 so the random number of mines is not less than the board size
+      generator = described_class.new(width: rand(5..100), height: rand(5..100), number_of_mines: rand(1...25))
+      height = generator.board.first.length 
+
+      sub_arrays_are_same_height = generator.board.all? { |sub_array| sub_array.length == height }
+
+      expect(sub_arrays_are_same_height).to be true
+    end
   end
 end
